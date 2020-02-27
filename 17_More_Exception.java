8f1864916c1fe38bdd6a21fbedeb1918ebe9f68f
Pradipta Sarma <38849757+pradiptasarma@users.noreply.github.com>
2020-02-27 15:36:52 +0530

    Update 17_More_Exception.java

--- 17_More_Exception.java
+++ 17_More_Exception.java
@@ -8,10 +8,9 @@ class Calculator{
         if (n < 0 || p<0){
             throw new Exception("n and p should be non-negative");
         }
-        else{
-            for (int i=0; i<p; i++){
-                pow = pow*n;
-            }
+        
+        for (int i=0; i<p; i++){
+            pow = pow*n;
         }
         return pow;
     }
